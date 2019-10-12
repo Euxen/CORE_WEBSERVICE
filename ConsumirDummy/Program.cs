5cc52101e7b3ca5a67dcb9d39ace06c1b1214b57
JunhuoHe <kelvinhezhang@live.com>
2019-10-12 00:49:39 -0400

    Hello! Soy un test del Web Service !

--- ConsumirDummy/Program.cs
+++ ConsumirDummy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsumirDummy.COREServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,28 @@ namespace ConsumirDummy
     {
         static void Main(string[] args)
         {
+            
+            COREServices.WebMainMenuSoapClient webMainMenu = new COREServices.WebMainMenuSoapClient();
+
+            COREServices.RequestClientRegister requestClientRegister = new COREServices.RequestClientRegister();
+            requestClientRegister.AutoGenerate = true;
+            requestClientRegister.Direction_Regs = "direction";
+            requestClientRegister.Email_Regs = "wsd@email";
+            requestClientRegister.Identifier = "12345678";
+            requestClientRegister.Lastname_Regs = "Doe";
+            requestClientRegister.Name_Regs = "John";
+            requestClientRegister.Password_Regs = "1234";
+            requestClientRegister.Pin_Regs = "";
+
+            COREServices.ResponseClientRegister responseClientRegister = new COREServices.ResponseClientRegister();
+
+            responseClientRegister = webMainMenu.ClientRegister(requestClientRegister);
+
+            Console.WriteLine(responseClientRegister.Message);
+            Console.ReadKey();
+
+
+            COREServices.RequestAccountRegister requestAccountRegister = new COREServices.RequestAccountRegister();
 
         }
     }
